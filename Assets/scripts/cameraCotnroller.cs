8360ef7504709452df12601ae2b477df9406b549
Will Brennan <galacticonrex@yahoo.com>
2017-01-23 19:48:27 -0500

    Game files added

---
+++ Assets/scripts/cameraCotnroller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class cameraCotnroller : MonoBehaviour {
+
+    public float defaultSize = 1.0f;
+    private Stack<hiddenEnvironment> environments;
+    private Camera mainCamera;
+
+    public void Push(hiddenEnvironment env)
+    {
+        environments.Push(env);
+    }
+    public void Pop()
+    {
+        environments.Pop();
+    }
+
+    void Start()
+    {
+        environments = new Stack<hiddenEnvironment>();
+        mainCamera = GetComponent<Camera>();
+    }
+	void Update ()
+    {
+        float target = (environments.Count == 0) ? defaultSize : environments.Peek().CameraSize;
+
+        float dif = (target - mainCamera.orthographicSize);
+        float delta = Mathf.Sign(dif) * 0.05f + dif / 12.0f;
+        mainCamera.orthographicSize += dif / 12.0f;
+    }
+}
---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+    public float speed = 0.25f;
+
+    private Rigidbody2D rigidBody;
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        hiddenEnvironment obj = other.GetComponent<hiddenEnvironment>();
+        if ( obj != null )
+            obj.SignalEnter();
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        hiddenEnvironment obj = other.GetComponent<hiddenEnvironment>();
+        if (obj != null)
+            obj.SignalExit();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        rigidBody.velocity = new Vector2(0, 0);
+
+	    if ( Input.GetKey(KeyCode.LeftArrow) )
+            rigidBody.velocity += new Vector2(-speed, 0);
+        if (Input.GetKey(KeyCode.RightArrow))
+            rigidBody.velocity += new Vector2( speed, 0);
+
+        mainCamera.transform.position =
+            new Vector3(transform.position.x, transform.position.y, -10);
+    }
+}
